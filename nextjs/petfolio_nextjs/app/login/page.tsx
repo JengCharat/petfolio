a63cbcbd17d414ecaf1c9ee145fd7fcbdb0d6fb6
JengCharat <zazajeng401@gmail.com>
2025-09-12 09:48:29 +0700

    login page
    /login

---
+++ nextjs/petfolio_nextjs/app/login/page.tsx
@@ -0,0 +1,9 @@
+import Navbar from "../components/Navbar"
+export default function Login(){
+    return(
+        <>
+            <Navbar/>
+            <h1>this is login page</h1>
+        </>
+    )
+}
