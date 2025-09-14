69398e6cc471e76133a6853038dfc535f2fa9854
KIPPON39 <kurakuri2104@gmail.com>
2025-09-15 00:36:27 +0700

    TestCommit02
    AddCommuPage

---
+++ nextjs/petfolio_nextjs/app/community/page.tsx
@@ -0,0 +1,17 @@
+
+import Navbar from "../components/Navbar";
+
+
+export default function Community(){
+    return (
+        <> 
+        <Navbar/>
+        <h1>This is Community Page</h1>
+
+
+        
+
+
+        </>
+    )
+}
