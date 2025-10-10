bea486635f5035ed7de7f506dc0233fa7f6212d6
Ginny421 <duangruethai.a@kkumail.com>
2025-10-11 06:20:30 +0700

    แก้ navbar หน่อย ((ใส่ฟ้อน))

--- nextjs/petfolio_nextjs/app/components/Navbar.tsx
+++ nextjs/petfolio_nextjs/app/components/Navbar.tsx
@@ -82,7 +82,7 @@ useEffect(() => {
   };
 
   return (
-    <nav className="bg-white/90 backdrop-blur-md shadow-lg sticky top-0 z-50 border-b border-purple-100">
+    <nav className=" font-sans bg-white/90 backdrop-blur-md shadow-lg sticky top-0 z-50 border-b border-purple-100">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between items-center h-16">
           {/* โลโก้ */}
